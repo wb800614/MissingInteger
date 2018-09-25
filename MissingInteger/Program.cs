64eea2bf62988e22ab3a6ae11bf6c21d9c094d5e
wb800614 <OhioU4me>
2018-09-25 01:07:57 -0400

    initial commit

---
+++ MissingInteger/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+public class Solution {
+    public int solution(int[] A) {
+        int smallest = 1;
+        if (A == null || A.Length <= 0)
+        {
+            return smallest;
+        }
+        Array.Sort(A);
+        for(int i = 0; i < A.Length; i++)
+        {
+            if (A[i] == smallest)
+                smallest++;
+        }
+        return smallest;
+    }
+}
